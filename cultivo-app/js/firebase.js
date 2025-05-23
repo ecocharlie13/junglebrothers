c58ac38fafde370212348a0f76bcbcb092ad32a7
ecocharlie13 <ecocharlie13@gmail.com>
2025-05-23 13:40:04 -0300

    Create firebase.js

---
+++ cultivo-app/js/firebase.js
@@ -0,0 +1,13 @@
+// js/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC07MoowfRaolBp0oBQo9YJ-4NL0CJYFKE",
+  authDomain: "jungle-brothers-938e0.firebaseapp.com",
+  projectId: "jungle-brothers-938e0",
+  storageBucket: "jungle-brothers-938e0.appspot.com",
+  messagingSenderId: "221354970870",
+  appId: "1:221354970870:web:a7f68c75480dc094bbad67"
+};
+
+export const app = initializeApp(firebaseConfig);
