35dcf7eb2ef4cf44a28037758e779f9af82c3ef1
ecocharlie13 <ecocharlie13@gmail.com>
2025-05-25 12:33:41 -0300

    Create firebase-init.js

---
+++ js/firebase-init.js
@@ -0,0 +1,19 @@
+// js/firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCo7MOVfaalrDg0o0GpYJ4-YNL4OCrjfXE",
+  authDomain: "jungle-brothers-93e80.firebaseapp.com",
+  projectId: "jungle-brothers-93e80",
+  storageBucket: "jungle-brothers-93e80.appspot.com",
+  messagingSenderId: "221354970870",
+  appId: "1:221354970870:web:a7f68c75480dc094bbad67"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { app, auth, db };
