419a3c02ca21eed26a1ecad56a23f8db3a05fca4
ecocharlie13 <ecocharlie13@gmail.com>
2025-05-26 08:56:08 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -2,11 +2,9 @@
 import { auth } from './firebase-init.js';
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Protege páginas da pasta /cultivoapp (ou outras privadas)
+// Protege páginas da pasta /cultivoapp
 onAuthStateChanged(auth, (user) => {
   if (!user) {
     window.location.href = "/login.html";
-  } else {
-    console.log("✅ Usuário autenticado:", user.email);
   }
 });
