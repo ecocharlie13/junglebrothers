6a2ad9e92fd6393dbbc4deea81c087968e5f201c
ecocharlie13 <ecocharlie13@gmail.com>
2025-05-26 09:25:50 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,10 +1,26 @@
 // js/auth.js
-import { auth } from './firebase-init.js';
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
+import { auth } from "./firebase-init.js";
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 
-// Protege páginas da pasta /cultivoapp
-onAuthStateChanged(auth, (user) => {
-  if (!user) {
-    window.location.href = "/login.html";
-  }
-});
+/**
+ * Verifica se o usuário está autenticado.
+ * Se sim, executa o callback. Senão, redireciona para o login.
+ * @param {Function} callback - Função a ser executada após verificação
+ */
+export function verificarLogin(callback) {
+  onAuthStateChanged(auth, (user) => {
+    if (!user) {
+      window.location.href = "/login.html";
+    } else {
+      callback(user);
+    }
+  });
+}
+
+/**
+ * Função para logout global
+ */
+export async function sair() {
+  await signOut(auth);
+  window.location.href = "/login.html";
+}
