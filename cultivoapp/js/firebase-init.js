95d35ce95421969fe9f2a9eb44f3f1e0628f3038
ecocharlie13 <ecocharlie13@gmail.com>
2025-05-27 16:18:17 -0300

    Update firebase-init.js

--- cultivoapp/js/firebase-init.js
+++ cultivoapp/js/firebase-init.js
@@ -1,4 +1,3 @@
-// js/firebase-init.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
