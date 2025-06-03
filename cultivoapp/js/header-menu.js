4262e1adb47a1d3113ca2d0c0314ea711efc0bf0
ecocharlie13 <ecocharlie13@gmail.com>
2025-06-03 13:13:15 -0300

    Create header-menu.js

---
+++ cultivoapp/js/header-menu.js
@@ -0,0 +1,11 @@
+// /cultivoapp/js/header-menu.js
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const menu = document.getElementById("mobile-menu");
+
+  if (toggle && menu) {
+    toggle.addEventListener("click", () => {
+      menu.classList.toggle("hidden");
+    });
+  }
+});
