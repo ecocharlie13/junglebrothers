920ead93302b11cf70e600de1f35f0d98f6eb0a9
ecocharlie13 <ecocharlie13@gmail.com>
2025-06-04 14:30:10 -0300

    Update firebase-init.js

--- cultivo-blocos/js/firebase-init.js
+++ cultivo-blocos/js/firebase-init.js
@@ -1,1 +1,17 @@
+// firebase-init.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore.js";
 
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "jungle-brothers-93e80.firebaseapp.com",
+  projectId: "jungle-brothers-93e80",
+  storageBucket: "jungle-brothers-93e80.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
